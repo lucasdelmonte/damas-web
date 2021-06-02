d80449dad610660a0805459e4b85e6eda430cb02
lucasdelmonte <delmontelucas678@gmail.com>
2021-06-02 19:55:15 -0300

    Se cargaron las damas en el tablero utilizando Javascript

--- tablero.js
+++ tablero.js
@@ -1,12 +1,12 @@
 var tableroArray = [
     [0,1,0,0,0,2,0,2],
-    [1,0,1,0,0,0,2,0],
-    [0,1,0,0,0,2,0,2],
-    [1,0,1,0,0,0,2,0],
-    [0,1,0,0,0,2,0,2],
-    [1,0,1,0,0,0,2,0],
-    [0,1,0,0,0,2,0,2],
-    [1,0,1,0,0,0,2,0]
+    [1,0,0,0,2,0,2,0],
+    [0,1,0,0,0,0,0,2],
+    [1,0,0,0,1,0,0,0],
+    [0,0,0,2,0,0,0,2],
+    [1,0,1,0,2,0,2,0],
+    [0,1,0,0,0,0,0,0],
+    [1,0,1,0,2,0,2,0]
 ];
 
 /* <---------------Comienza la parte del TABLERO---------------> */
@@ -33,5 +33,27 @@ for (var i = 0; i < tableroArray.length; i++) {
             nuevoDiv.id = 'columna-' + i + '-fila-' + j;
             nuevoDivColumna.appendChild(nuevoDiv);
         }
+}
+
+/* <---------------Comienza la parte de las DAMAS---------------> */
+
+for (var i = 0; i < tableroArray.length; i++) {  
+
+    for (var v = 0; v < tableroArray[i].length; v++) { 
+
+        if (tableroArray[i][v] === 1) {
+            var damaBlanca = document.createElement('img');
+            damaBlanca.src = 'img/ficha_blanca.png';
+            damaBlanca.alt = 'ficha_blanca';
+            document.getElementById('columna-' + i +'-fila-' + v).appendChild(damaBlanca);
+        }else{
+            if (tableroArray[i][v] === 2) {
+                var damaRoja = document.createElement('img');
+                damaRoja.src = 'img/ficha_roja.png';
+                damaRoja.alt = 'ficha_blanca';
+                document.getElementById('columna-' + i +'-fila-' + v).appendChild(damaRoja);
+            }
+        }
+    }
 }
 
