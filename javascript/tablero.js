791c09266202b47edbc5db8895b8d3f6611af778
lucasdelmonte <delmontelucas678@gmail.com>
2021-06-17 17:58:02 -0300

    Se le agrego un tono verde a la casilla que puede desplazarse la dama segun su posicion (movimiento en diagonal)

--- javascript/tablero.js
+++ javascript/tablero.js
@@ -1,12 +1,12 @@
 var tableroArray = [
     [0,1,0,1,0,1,0,1],
-    [1,0,1,0,0,0,1,0],
-    [0,0,0,0,0,1,0,1],
-    [0,0,0,0,2,0,0,0],
-    [0,2,0,1,0,2,0,2],
-    [2,0,0,0,0,0,0,0],
-    [0,2,0,0,0,2,0,2],
-    [2,0,2,0,2,0,0,0]
+    [1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [2,0,2,0,2,0,2,0],
+    [0,2,0,2,0,2,0,2],
+    [2,0,2,0,2,0,2,0]
 ];
 
 var casilla, piezaMovil, piezaMovilSeleccionada, turno = 1, posicion;
@@ -34,13 +34,10 @@ for (var i = 0; i < tableroArray.length; i++) {
             casilla.id = 'fila-' + i + '-columna-' + j;
             nuevoElementoTr.appendChild(casilla);                   
         }
-
-        var casillas = document.querySelectorAll('td'); 
-
-        for(var x = 0; x < casillas.length; x++) {
-            casillas[x].addEventListener('click', seleccionaPieza);
-        }
 }
+
+
+
 
 /* <---------------Comienza la parte de las DAMAS---------------> */
 
@@ -66,30 +63,9 @@ for (var i = 0; i < tableroArray.length; i++) {
     }
 }
 
-/* <---------------Comienza la parte de los JUGADORES---------------> */
+/* <---------------Comienza la parte de enfocar, mover las DAMAS y TURNOS de los jugadores---------------> */
 
-document.getElementById('jugador1').innerHTML = prompt('Ingrese el nombre del primero jugador:');
-document.getElementById('jugador2').innerHTML = prompt('Ingrese el nombre del segundo jugador:');
-
-/* <---------------Comienza la parte del CONTADOR---------------> */
-
-document.getElementById('jugador1_puntos').innerHTML = 1;
-document.getElementById('jugador2_puntos').innerHTML = 2;
-
-/* <---------------Comienza la parte de los TURNOS---------------> */
-
-var jugador1 = document.getElementById('jugador1'), jugador2 = document.getElementById('jugador2');
-
-/* El turno le corresponde al jugador que tenga el color #33ff33 */
-jugador1.style.borderBottom = '4px solid #33ff33';
-jugador1.style.borderRadius = '10px';
-
-jugador2.style.borderBottom = '4px solid #c4c4c4';
-jugador2.style.borderRadius = '10px';
-
-/* <---------------Comienza la parte de enfocar y mover las DAMAS---------------> */
-
-function seleccionaPieza() {   
+/* function seleccionaPieza() {   
     if (turno == 1) {
         if(!piezaMovilSeleccionada && this.firstElementChild) {   
             casilla = this; 
@@ -121,4 +97,139 @@ function seleccionaPieza() {
             } 
         }
     }
+} */
+
+function seleccionaPieza() {
+    switch (turno) {
+        case 1:
+            jugador1.style.borderBottom = '4px solid #33ff33';
+            jugador1.style.borderRadius = '10px';
+            if(!piezaMovilSeleccionada && this.firstElementChild) {   
+                casilla = this; 
+                piezaMovil = this.innerHTML;
+                this.querySelector('img[alt="ficha_blanca"]').classList.add('pintado');
+                
+                //Movimientos posibles
+                ubicacion = this.id;
+                fila = ubicacion.substring(5, 6); 
+                columna = ubicacion.substring(15);
+
+                if(columna == 7){ 
+                    fila++;  
+                    columna--;
+                    ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                    ubicacionFinalUno.classList.add('movimiento');
+                } else {
+                    if(columna == 0){
+                        fila++;  
+                        columna++;   
+                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                        ubicacionFinalUno.classList.add('movimiento');
+                    } else {
+                        fila++;  
+                        columna++;   
+                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                        ubicacionFinalUno.classList.add('movimiento');
+                        columna = columna - 2;
+                        ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                        ubicacionFinalDos.classList.add('movimiento');
+                    }
+                }
+
+                piezaMovilSeleccionada = true;
+            } else if (piezaMovilSeleccionada){
+                posicion = this;
+                casilla.innerHTML= '';
+                piezaMovilSeleccionada = false;
+                this.innerHTML = piezaMovil;
+
+
+                columna = columna + 2;
+                if(columna != 8){ 
+                    ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+                }
+                columna = columna - 2;
+                ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+
+                if(posicion != casilla){
+                    turno = 2;
+                    jugador1.style.borderBottom = '4px solid #c4c4c4';
+                    jugador2.style.borderBottom = '4px solid #33ff33';
+                } 
+            }  
+        break;
+        
+        case 2:
+            if(!piezaMovilSeleccionada && this.firstElementChild) {   
+                casilla = this; 
+                piezaMovil = this.innerHTML; 
+                this.querySelector('img[alt="ficha_roja"]').classList.add('pintado');
+
+                //Movimientos posibles
+                ubicacion = this.id;
+                fila = ubicacion.substring(5, 6); 
+                columna = ubicacion.substring(15);
+                ubicacionInicial = document.querySelector('#fila-' + fila +'-columna-' + columna );
+
+                if(columna == 7){ 
+                    fila--;  
+                    columna--;
+                    ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                    ubicacionFinalUno.classList.add('movimiento');
+                } else {
+                    if(columna == 0){
+                        fila--;  
+                        columna++;   
+                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                        ubicacionFinalUno.classList.add('movimiento');
+                    } else {
+                        fila--;  
+                        columna--;   
+                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                        ubicacionFinalUno.classList.add('movimiento');
+                        columna = columna + 2;
+                        ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                        ubicacionFinalDos.classList.add('movimiento');
+                    }
+                }
+
+                piezaMovilSeleccionada = true;
+            } else if(piezaMovilSeleccionada){
+                posicion = this;
+                casilla.innerHTML= '';
+                piezaMovilSeleccionada = false;
+                this.innerHTML = piezaMovil;
+
+                if(columna != 1){ 
+                    columna = columna - 2;
+                    ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+                    
+                    columna = columna + 2;
+                    ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+
+                }  
+                ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+
+                if(posicion != casilla){
+                    turno = 1;
+                    jugador2.style.borderBottom = '4px solid #c4c4c4';
+                    jugador1.style.borderBottom = '4px solid #33ff33';
+                } 
+            } 
+        break;
+
+        default:
+        break;
+    }
 }
+
+var casillas = document.querySelectorAll('td'); 
+
+for(var x = 0; x < casillas.length; x++) {
+    casillas[x].addEventListener('click', seleccionaPieza);
+}
+
+/* <---------------Comienza la parte de los nombres para los JUGADORES---------------> */
+
+document.getElementById('jugador1').innerHTML = prompt('Ingrese el nombre del primero jugador:');
+document.getElementById('jugador2').innerHTML = prompt('Ingrese el nombre del segundo jugador:');
