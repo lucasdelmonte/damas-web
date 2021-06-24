bc5e7a84bccaa6548996531c615af65f7d6d4e00
lucasdelmonte <delmontelucas678@gmail.com>
2021-06-23 22:18:54 -0300

    Se crearon 2 funciones para controlar el movimiento de las damas y dejar mas simple la funcion del click

--- javascript/tablero.js
+++ javascript/tablero.js
@@ -1,11 +1,11 @@
 var tableroArray = [
     [0,1,0,1,0,1,0,1],
-    [1,0,1,0,1,0,1,0],
-    [0,1,0,1,0,1,0,1],
+    [1,0,1,0,1,0,0,0],
+    [0,0,0,1,0,1,0,1],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
-    [2,0,2,0,2,0,2,0],
-    [0,2,0,2,0,2,0,2],
+    [2,0,2,0,2,0,0,0],
+    [0,0,0,2,0,2,0,2],
     [2,0,2,0,2,0,2,0]
 ];
 
@@ -74,57 +74,30 @@ function seleccionaPieza() {
                 piezaMovil = this.innerHTML;
                 this.querySelector('img[alt="ficha_blanca"]').classList.add('pintado');
 
-                //Movimientos posibles activados
+                //Movimientos posibles
                 ubicacion = this.id;
                 fila = ubicacion.substring(5, 6); 
                 columna = ubicacion.substring(15);
-                
-                if(columna == 7){ 
-                    fila++;  
-                    columna--;
-                    ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
-                    ubicacionFinalUno.classList.add('movimiento');
-                } else {
-                    if(columna == 0){
-                        fila++;  
-                        columna++;   
-                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
-                        ubicacionFinalUno.classList.add('movimiento');
-                    } else {
-                        fila++;  
-                        columna++;   
-                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
-                        ubicacionFinalUno.classList.add('movimiento');
-                        columna = columna - 2;
-                        ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna );
-                        ubicacionFinalDos.classList.add('movimiento');
-                    }
-                }
-
+                celda = true;
+                movimientoBlanca(fila, columna);
                 movimiento = document.querySelectorAll('.movimiento');
 
                 piezaMovilSeleccionada = true;
-            } else if (piezaMovilSeleccionada && !this.querySelector('img[alt="ficha_blanca"]') && !this.querySelector('img[alt="ficha_roja"]')){
+            } else if (piezaMovilSeleccionada){
                 posicion = this; 
-
-                if(posicion != casilla && posicion.id === movimiento[0].id || posicion.id === movimiento[1].id){
+                if(posicion != casilla && posicion.id === movimiento[0].id && !this.firstElementChild || posicion.id === movimiento[1].id && !this.firstElementChild){
+                    celda = false;
                     casilla.innerHTML= '';
                     piezaMovilSeleccionada = false;
                     this.innerHTML = piezaMovil;
-                    turno = 2;
 
+                    turno = 2;
                     //Quitamos los estilos jugador 1
                     jugador1.style.borderBottom = '4px solid #c4c4c4';
                     //Agregamos los estilos jugador 2
                     jugador2.style.borderBottom = '4px solid #33ff33';
-                    
-                    //Se remueve el efecto en las casillas porque ya movio la dama
-                    columna = columna + 2;
-                    if(columna != 8){ 
-                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
-                    }
-                    columna = columna - 2;
-                    ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento'); 
+
+                    movimientoBlanca(fila, columna);
                 }
             }
         break;
@@ -136,59 +109,31 @@ function seleccionaPieza() {
                 piezaMovil = this.innerHTML; 
                 this.querySelector('img[alt="ficha_roja"]').classList.add('pintado');
 
-                //Movimientos posibles activados
+                //Movimientos posibles
                 ubicacion = this.id;
                 fila = ubicacion.substring(5, 6); 
                 columna = ubicacion.substring(15);
-
-                if(columna == 7){ 
-                    fila--;  
-                    columna--;
-                    ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
-                    ubicacionFinalUno.classList.add('movimiento');
-                } else {
-                    if(columna == 0){
-                        fila--;  
-                        columna++;   
-                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
-                        ubicacionFinalUno.classList.add('movimiento');
-                    } else {
-                        fila--;  
-                        columna--;   
-                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
-                        ubicacionFinalUno.classList.add('movimiento');
-                        columna = columna + 2;
-                        ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna );
-                        ubicacionFinalDos.classList.add('movimiento');
-                    }
-                }
+                celda = true;
+                movimientoRoja(fila, columna);
 
                 movimiento = document.querySelectorAll('.movimiento');
 
                 piezaMovilSeleccionada = true;
-            } else if(piezaMovilSeleccionada && !this.querySelector('img[alt="ficha_roja"]') && !this.querySelector('img[alt="ficha_blanca"]')){
+            } else if(piezaMovilSeleccionada){
                 posicion = this; 
-
-                if(posicion != casilla && posicion.id === movimiento[0].id || posicion.id === movimiento[1].id){
+                if(posicion != casilla && posicion.id === movimiento[0].id && !this.firstElementChild || posicion.id === movimiento[1].id && !this.firstElementChild){
+                    celda = false;
                     casilla.innerHTML= '';
                     piezaMovilSeleccionada = false;
                     this.innerHTML = piezaMovil;
-                    turno = 1;
 
+                    turno = 1;
                     //Quitamos los estilos jugador 2
                     jugador2.style.borderBottom = '4px solid #c4c4c4';
                     //Agregamos los estilos jugador 1
                     jugador1.style.borderBottom = '4px solid #33ff33';
 
-                    //Se remueve el efecto en las casillas porque ya movio la dama
-                    if(columna != 1){ 
-                        columna = columna - 2;
-                        ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
-
-                        columna = columna + 2;
-                        ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
-                    }  
-                    ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento'); 
+                    movimientoRoja(fila, columna);
                 }
             }
         break;
@@ -208,3 +153,116 @@ for(var x = 0; x < casillas.length; x++) {
 
 document.getElementById('jugador1').innerHTML = prompt('Ingrese el nombre del primero jugador:');
 document.getElementById('jugador2').innerHTML = prompt('Ingrese el nombre del segundo jugador:');
+
+/* <---------------Comienza la parte del MENU DESPLEGABLE---------------> */
+
+var $btnMenu = document.querySelector('img[alt="Menu"]'), desplegado = true;
+
+$btnMenu.addEventListener('click', menuDesplegable);
+
+function menuDesplegable() {
+    if (desplegado) {
+        $enlace = document.querySelectorAll('.nav');
+    
+        for (var i = 0; i < $enlace.length; i++) {
+            $enlace[i].classList.remove('oculto');
+            $enlace[i].classList.add('mostrado');
+        } 
+
+        var $nav = document.getElementById('nav').style.height = '268px';
+
+        desplegado = false;
+    } else {
+        for (var i = 0; i < $enlace.length; i++) {
+            $enlace[i].classList.remove('mostrado');
+            $enlace[i].classList.add('oculto');
+        } 
+
+        var $nav = document.getElementById('nav').style.height = '120px';
+
+        desplegado = true;
+    }
+}
+
+/* <---------------Comienza la parte de los MOVIMIENTOS VALIDOS---------------> */
+
+function movimientoBlanca(fila, columna){
+    if (celda) {
+        //Se coloca un efecto en las casillas donde puede mover la dama
+        if(columna == 7){ 
+            fila++;  
+            columna--;
+            ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+            ubicacionFinalUno.classList.add('movimiento');
+        } else {
+            if(columna == 0){
+                fila++;
+                columna++;
+                ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                ubicacionFinalUno.classList.add('movimiento');
+            } else {
+                fila++;  
+                columna++;   
+                ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                ubicacionFinalUno.classList.add('movimiento');
+                
+                columna = columna - 2;
+                ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                ubicacionFinalDos.classList.add('movimiento');
+            }
+        }
+    } else {
+        //Se remueve el efecto en las casillas porque ya movio la dama
+        fila++;
+        columna++;
+        if(columna != 8){ 
+            ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+        }
+        columna = columna - 2;
+        ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+    }
+}
+
+function movimientoRoja(fila, columna){
+    if (celda) {
+        //Se coloca un efecto en las casillas donde puede mover la dama
+        if(columna == 7){ 
+            fila--;  
+            columna--;
+            ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+            ubicacionFinalUno.classList.add('movimiento');
+        } else {
+            if(columna == 0){
+                fila--;  
+                columna++;   
+                ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                ubicacionFinalUno.classList.add('movimiento');
+            } else {
+                fila--;  
+                columna--;   
+                ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                ubicacionFinalUno.classList.add('movimiento');
+                columna = columna + 2;
+                ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna );
+                ubicacionFinalDos.classList.add('movimiento');
+            }
+        }
+    } else {
+        //Se remueve el efecto en las casillas porque ya movio la dama
+        fila--;
+        if (columna == 0) {
+            columna++;
+        } else {
+            if (columna == 8) {
+                columna--;
+            } else {
+                columna--;
+                ubicacionFinalUno = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+
+                columna = columna + 2;
+                ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento');
+            }
+        }
+        ubicacionFinalDos = document.querySelector('#fila-' + fila +'-columna-' + columna ).classList.remove('movimiento'); 
+    }
+}
